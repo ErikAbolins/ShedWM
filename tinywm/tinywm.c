84d3fc0dccd46db69efb86b98a4f6e4e82d7a9d6
Erik <erik@overlord>
2026-01-30 22:20:10 +0000

    Clean up

--- tinywm/tinywm.c
+++ tinywm/tinywm.c
@@ -1,202 +1,140 @@
-/*
- * This software is in the public domain
- * and is provided AS IS, with NO WARRANTY.
- */
-
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
-#define MOD Mod4Mask
 #define MAX_WINDOWS 50
 #define MAX_WORKSPACES 9
+#define MOD Mod4Mask
 
 typedef struct {
     Window win;
-    int x, y, w, h;
 } Client;
 
 Client workspaces[MAX_WORKSPACES][MAX_WINDOWS];
-int workspace_counts[MAX_WORKSPACES] = {0};
-int current_workspace = 0;
+int win_counts[MAX_WORKSPACES] = {0};
+int curr = 0;
+Display *dpy;
+Window root;
+Atom wm_delete;
 
-#define clients (workspaces[current_workspace])
-#define nclients (workspace_counts[current_workspace])
+void tile(int ws) {
+    int n = win_counts[ws];
+    if (n == 0) return;
 
-void add_client(Window win) {
-    if (nclients >= MAX_WINDOWS) return;
-    clients[nclients].win = win;
-    nclients++;
-    fprintf(stderr, "Added client %lu to workspace %d\n", win, current_workspace);
-}
+    int sw = DisplayWidth(dpy, DefaultScreen(dpy));
+    int sh = DisplayHeight(dpy, DefaultScreen(dpy));
 
-void remove_client(Window win){
-    for(int i = 0; i < nclients; i++){
-        if(clients[i].win == win) {
-            for(int j = i; j < nclients - 1; j++){
-                clients[j] = clients[j + 1];
-            }
-            nclients--;
-            fprintf(stderr, "Removed client %lu from workspace %d\n", win, current_workspace);
-            return;
-        }
-    }
-}
-
-void tile(Display *dpy, int screen_w, int screen_h){
-    if(nclients == 0) return;
-
-    if(nclients == 1) {
-        XMoveResizeWindow(dpy, clients[0].win, 0, 0, screen_w, screen_h);
+    if (n == 1) {
+        XMoveResizeWindow(dpy, workspaces[ws][0].win, 0, 0, sw, sh);
     } else {
-        int master_w = screen_w / 2;
-        int stack_w = screen_w - master_w;
-        int stack_h = screen_h / (nclients - 1);
+        int mw = sw / 2;
+        XMoveResizeWindow(dpy, workspaces[ws][0].win, 0, 0, mw, sh);
+        int th = sh / (n - 1);
+        for (int i = 1; i < n; i++) {
+            XMoveResizeWindow(dpy, workspaces[ws][i].win, mw, (i - 1) * th, sw - mw, th);
+        }
+    }
+    XSync(dpy, False);
+}
 
-        XMoveResizeWindow(dpy, clients[0].win, 0, 0, master_w, screen_h);
+void add_client(Window w) {
+    if (win_counts[curr] >= MAX_WINDOWS) return;
+    // Don't manage the root window or random nulls
+    if (w == None || w == root) return;
+    
+    workspaces[curr][win_counts[curr]++].win = w;
+    XSelectInput(dpy, w, EnterWindowMask | FocusChangeMask);
+}
 
-        for(int i = 1; i < nclients; i++) {
-            XMoveResizeWindow(dpy, clients[i].win,
-                master_w, (i - 1) * stack_h,
-                stack_w, stack_h);
+void remove_client(Window w) {
+    for (int ws = 0; ws < MAX_WORKSPACES; ws++) {
+        for (int i = 0; i < win_counts[ws]; i++) {
+            if (workspaces[ws][i].win == w) {
+                for (int j = i; j < win_counts[ws] - 1; j++) workspaces[ws][j] = workspaces[ws][j + 1];
+                win_counts[ws]--;
+                if (ws == curr) tile(ws);
+                return;
+            }
         }
     }
 }
 
-int main(void)
-{
-    Display *dpy;
-    XWindowAttributes attr;
-    XButtonEvent start;
+void kill_client(Window w) {
+    if (w == None || w == root) return;
     XEvent ev;
-    Window focused = None;
+    ev.type = ClientMessage;
+    ev.xclient.window = w;
+    ev.xclient.message_type = XInternAtom(dpy, "WM_PROTOCOLS", False);
+    ev.xclient.format = 32;
+    ev.xclient.data.l[0] = wm_delete;
+    ev.xclient.data.l[1] = CurrentTime;
+    XSendEvent(dpy, w, False, NoEventMask, &ev);
+}
 
-    if(!(dpy = XOpenDisplay(NULL))) return 1;
-    XSelectInput(dpy, DefaultRootWindow(dpy),
-        SubstructureRedirectMask | SubstructureNotifyMask);
+void goto_workspace(int next) {
+    if (next == curr || next < 0 || next >= MAX_WORKSPACES) return;
+    for (int i = 0; i < win_counts[curr]; i++) XUnmapWindow(dpy, workspaces[curr][i].win);
+    curr = next;
+    for (int i = 0; i < win_counts[curr]; i++) XMapWindow(dpy, workspaces[curr][i].win);
+    tile(curr);
+}
 
-    XGrabKey(dpy, XKeysymToKeycode(dpy, XK_Return), MOD,
-        DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+int main(void) {
+    XEvent ev;
+    if (!(dpy = XOpenDisplay(NULL))) return 1;
+    root = DefaultRootWindow(dpy);
+    wm_delete = XInternAtom(dpy, "WM_DELETE_WINDOW", False);
 
-    XGrabKey(dpy, 24, MOD | ShiftMask,
-        DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+    XSelectInput(dpy, root, SubstructureRedirectMask | SubstructureNotifyMask);
 
-    XGrabKey(dpy, 27, MOD | ShiftMask,
-        DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+    // Keybindings
+    XGrabKey(dpy, XKeysymToKeycode(dpy, XK_Return), MOD, root, True, GrabModeAsync, GrabModeAsync);
+    XGrabKey(dpy, XKeysymToKeycode(dpy, XK_q), MOD | ShiftMask, root, True, GrabModeAsync, GrabModeAsync);
+    XGrabKey(dpy, XKeysymToKeycode(dpy, XK_r), MOD | ShiftMask, root, True, GrabModeAsync, GrabModeAsync);
+    XGrabKey(dpy, XKeysymToKeycode(dpy, XK_d), MOD, root, True, GrabModeAsync, GrabModeAsync);
 
-    for(int i = 1; i <= 9; i++) {
-        char numkey[2];
-        sprintf(numkey, "%d", i);
-        XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym(numkey)), MOD,
-            DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+    for (int i = 1; i <= 9; i++) {
+        XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym((char[]){i + '0', 0})), MOD, root, True, GrabModeAsync, GrabModeAsync);
     }
 
-    XGrabKey(dpy, 40, MOD, DefaultRootWindow(dpy),
-        True, GrabModeAsync, GrabModeAsync);
+    while (!XNextEvent(dpy, &ev)) {
+        if (ev.type == MapRequest) {
+            XWindowAttributes wa;
+            XGetWindowAttributes(dpy, ev.xmaprequest.window, &wa);
+            if (wa.override_redirect) continue; // Don't manage tooltips/menus
 
-    start.subwindow = None;
-
-    for(;;) {
-        XNextEvent(dpy, &ev);
-
-        if(ev.type == KeyPress) {
-            if(ev.xkey.state == MOD &&
-               ev.xkey.keycode == XKeysymToKeycode(dpy, XK_Return)) {
-                if(fork() == 0) {
-                    execl("/usr/local/bin/st", "st", NULL);
-                    exit(0);
-                }
-            }
-
-            else if(ev.xkey.state == (MOD | ShiftMask) && ev.xkey.keycode == 24) {
-                if(focused != None) {
-                    XDestroyWindow(dpy, focused);
-                    XFlush(dpy);
-                    remove_client(focused);
-                    focused = None;
-                    
-                    Screen *s = DefaultScreenOfDisplay(dpy);
-                    tile(dpy, s->width, s->height);
-                }
-            }
-
-            else if(ev.xkey.state == (MOD | ShiftMask) && ev.xkey.keycode == 27) {
-                execvp("./tinywm", (char *[]){"./tinywm", NULL});
-            }
-
-            else if(ev.xkey.state == MOD && ev.xkey.keycode == 40) {
-                if(fork() == 0) {
-                    execl("/usr/bin/dmenu_run", "dmenu_run", NULL);
-                    exit(0);
-                }
-            }
-
-            for(int i = 1; i <= 9; i++) {
-                char numkey[2];
-                sprintf(numkey, "%d", i);
-
-                if(ev.xkey.keycode ==
-                   XKeysymToKeycode(dpy, XStringToKeysym(numkey))) {
-
-                    // Save old workspace
-                    int old_workspace = current_workspace;
-
-                    // Hide OLD workspace windows
-                    for(int j = 0; j < workspace_counts[old_workspace]; j++)
-                        XUnmapWindow(dpy, workspaces[old_workspace][j].win);
-
-                    // Switch to new workspace
-                    current_workspace = i - 1;
-                    focused = None;  // Clear focus when switching workspaces
-
-                    fprintf(stderr, "Switched to workspace %d\n", current_workspace);
-
-                    // Show NEW workspace windows
-                    for(int j = 0; j < workspace_counts[current_workspace]; j++)
-                        XMapWindow(dpy, workspaces[current_workspace][j].win);
-
-                    Screen *s = DefaultScreenOfDisplay(dpy);
-                    tile(dpy, s->width, s->height);
-                    break;  // Don't check other numbers
-                }
-            }
-        }
-
-        else if(ev.type == MapRequest) {
-            XMapWindow(dpy, ev.xmaprequest.window);
             add_client(ev.xmaprequest.window);
-
-            Screen *s = DefaultScreenOfDisplay(dpy);
-            tile(dpy, s->width, s->height);
+            XMapWindow(dpy, ev.xmaprequest.window);
+            tile(curr);
+        } 
+        else if (ev.type == DestroyNotify || ev.type == UnmapNotify) {
+            Window w = (ev.type == DestroyNotify) ? ev.xdestroywindow.window : ev.xunmap.window;
+            remove_client(w);
+        } 
+        else if (ev.type == EnterNotify) {
+            if (ev.xcrossing.window != None && ev.xcrossing.window != root) {
+                XSetInputFocus(dpy, ev.xcrossing.window, RevertToParent, CurrentTime);
+            }
+        } 
+        else if (ev.type == KeyPress) {
+            KeySym keysym = XLookupKeysym(&ev.xkey, 0);
+            if (keysym == XK_Return && (ev.xkey.state & MOD)) {
+                if (fork() == 0) execlp("st", "st", NULL), exit(0);
+            } else if (keysym == XK_q && (ev.xkey.state & (MOD | ShiftMask))) {
+                Window focused; int revert;
+                XGetInputFocus(dpy, &focused, &revert);
+                kill_client(focused);
+            } else if (keysym == XK_r && (ev.xkey.state & (MOD | ShiftMask))) {
+                execvp("./tinywm", (char *[]){"./tinywm", NULL});
+            } else if (keysym == XK_d && (ev.xkey.state & MOD)) {
+                if (fork() == 0) execlp("dmenu_run", "dmenu_run", NULL), exit(0);
+            } else if (keysym >= XK_1 && keysym <= XK_9) {
+                goto_workspace(keysym - XK_1);
+            }
         }
-
-        else if(ev.type == DestroyNotify) {
-            remove_client(ev.xdestroywindow.window);
-
-            Screen *s = DefaultScreenOfDisplay(dpy);
-            tile(dpy, s->width, s->height);
-        }
-
-        else if(ev.type == ButtonPress && ev.xbutton.subwindow != None) {
-            focused = ev.xbutton.subwindow;
-            XGetWindowAttributes(dpy, focused, &attr);
-            start = ev.xbutton;
-        }
-
-        else if(ev.type == MotionNotify && start.subwindow != None) {
-            int xdiff = ev.xbutton.x_root - start.x_root;
-            int ydiff = ev.xbutton.y_root - start.y_root;
-            XMoveResizeWindow(dpy, start.subwindow,
-                attr.x + (start.button==1 ? xdiff : 0),
-                attr.y + (start.button==1 ? ydiff : 0),
-                MAX(1, attr.width  + (start.button==3 ? xdiff : 0)),
-                MAX(1, attr.height + (start.button==3 ? ydiff : 0)));
-        }
-
-        else if(ev.type == ButtonRelease)
-            start.subwindow = None;
     }
+    return 0;
 }
